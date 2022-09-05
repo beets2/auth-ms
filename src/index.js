3dc704b1c8594740d109642222a2a598a555d7bc
Le4o <devleonardoandrade@gmail.com>
2022-09-05 19:58:15 -0300

    fix: dotenv config order import

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+dotenv.config();
+
 const { generalSecrets } = require("./config");
 const routes = require("./routes").default;
-dotenv.config();
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use("/", routes);
+
+console.log(generalSecrets);
 
 app.listen(generalSecrets.PORT, () =>
   console.log("Auth ms running at " + generalSecrets.PORT)
